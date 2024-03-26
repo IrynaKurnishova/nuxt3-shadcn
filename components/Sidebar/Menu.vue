242233c4f3902106c05b2ca39a22ef431b65cb1f
irinakurnishova <kurnishovaiv@gmail.com>
2024-03-26 14:49:05 +0100

    sidebar created

---
+++ components/Sidebar/Menu.vue
@@ -0,0 +1,53 @@
+<template>
+<div>
+  <header class="items-center flex gap-2 p-4 hover:scale-[101%] transition cursor-pointer">
+    <Logo/>
+    <p class="font-bold text-black">Nuxt Project</p>
+  </header>
+  <div class="px-4 grow">
+    <div class="grid gap-2">
+      <NuxtLink
+          :href="item.path"
+          v-for="(item, index) in items"
+          :key="index"
+          class="flex items-center gap-2 px-2 py-1 transition rounded cursor-pointer hover:bg-neutral-100"
+      >
+        <Icon size="20" :name="item.icon" color="black"/>
+        <span>{{item.title}}</span>
+      </NuxtLink>
+    </div>
+  </div>
+</div>
+</template>
+
+<script setup lang="ts">
+const items= ref([
+  {
+    title: "Overview",
+    path: "/",
+    icon: "ri:dashboard-line"
+  },
+  {
+    title: "Transactions",
+    path: "/transactions",
+    icon: "clarity:two-way-arrows-line"
+  },
+  {
+    title: "Account",
+    path: "/account",
+    icon: "solar:settings-minimalistic-linear"
+  },
+  {
+    title: "Contacts",
+    path: "/contacts",
+    icon: "mdi:account-box-outline"
+  },
+  {
+    title: "Settings",
+    path: "/settings",
+    icon: "ic:outline-settings"
+  },
+
+])
+</script>
+
---
+++ components/Sidebar/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div class="flex z-50 items-center justify-between w-full h-full p-4 lg:hidden">
+      <Logo/>
+      <Icon
+          @click="open = true"
+          class="z-50 cursor-pointer left-2 top-2"
+          size="30"
+          name="iconamoon:menu-burger-horizontal"
+      />
+      <div
+          v-if="open"
+          class="fixed top-0 left-0 z-[999] bg-white w-full h-full h-screen"
+      >
+        <Icon
+            @click="open = false"
+            class="absolute z-50 cursor-pointer right-4 top-4"
+            size="30"
+            name="material-symbols:close"
+        />
+        <SidebarMenu/>
+      </div>
+    </div>
+    <div class="hidden lg:flex w-[250px] h-screen flex flex-col justify-between border-r">
+      <SidebarMenu/>
+      <div>
+        UserItem
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const open = ref(true)
+</script>
