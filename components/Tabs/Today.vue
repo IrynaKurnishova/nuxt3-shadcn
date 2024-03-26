dfc3468ca271b8b8319e7df4b74c0472504677c1
irinakurnishova <kurnishovaiv@gmail.com>
2024-03-26 14:49:41 +0100

    another components for example created

---
+++ components/Tabs/Today.vue
@@ -0,0 +1,4 @@
+<template>
+<div>Hello today</div>
+</template>
+
---
+++ pages/transactions.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <header>
+      This is my header
+    </header>
+
+    <main>
+      This is my main
+      <section>
+
+      </section>
+    </main>
+
+    <footer>
+      This is my footer
+    </footer>
+  </div>
+</template>
+<script setup lang="ts">
+</script>
